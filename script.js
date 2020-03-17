f00beb97500aeb762c544c6acc318d75e1047eea
developingdipen <60754812+developingdipen@users.noreply.github.com>
2020-03-17 16:09:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,8 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Refresh scrolls to the top
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
